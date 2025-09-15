4c1682eb5dc2df783d7fccac9fbbff0a0d704cae
Travis Bassano <travis.bassano@gmail.com>
2025-09-15 15:27:00 -0500

    Simplifying sort annotation

--- assets/js/tablesort-init.js
+++ assets/js/tablesort-init.js
@@ -1,26 +1,8 @@
-// tablesort-init.js
+// assets/js/tablesort-init.js
 
 document.addEventListener('DOMContentLoaded', function() {
   const table = document.getElementById('myTable');
-  const ts = new Tablesort(table);
-
-  // Remove arrow classes from all headers
-  function clearArrowClasses() {
-    table.querySelectorAll('th').forEach(th => {
-      th.classList.remove('tablesort-up', 'tablesort-down');
-    });
+  if (table) {
+    new Tablesort(table);
   }
-
-  // Listen to sort event
-  table.addEventListener('tablesort:afterSort', function(event) {
-    clearArrowClasses();
-
-    const th = event.target; // the column that was sorted
-    if (ts.direction === 1) {
-      th.classList.add('tablesort-up');
-    } else if (ts.direction === -1) {
-      th.classList.add('tablesort-down');
-    }
-  });
 });
-
